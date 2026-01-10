bd46e964a2e5d61346cc0646ebf0626d9b306a0b
durgadevi-spec <durgadevi@ctint.in>
2026-01-10 10:38:22 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,8 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./lib/auth-context";
 import { DataProvider } from "./lib/store";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "./components/ui/toaster";
+
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Login from "@/pages/Login";
